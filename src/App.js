89efae8e9e70bc687104aee5c47301cb22d46ebe
Brahm <brahm.kelly@gmail.com>
2022-05-12 16:50:36 -0400

    buttons and things

--- src/App.js
+++ src/App.js
@@ -4,15 +4,32 @@ import './App.css';
 function App() {
 
   const [count, setCount] = useState(0);
+  const [firstName, setFirstName] = useState("Brahm");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const loginLogout = () => setIsLoggedIn(!isLoggedIn);
+
+ 
   return (
     <div className="App">
       <header className="App-header">
       
       
       <h1>You clicked the button {count} times.</h1>
-      <button onClick={()=> setCount(count + 1)}>Click me</button>
+      <button onClick={()=> setCount(count + 1)}> + </button>&nbsp;
+      <button onClick={()=> setCount(0)}> RESET </button>&nbsp;
+      <button onClick={()=> {if (count > 0) {setCount(count - 1)}}}> - </button>
+      
+      
+      <h2>Welcome { isLoggedIn ?  firstName : 'Guest'}</h2>
+      <button onClick={loginLogout}>{isLoggedIn ?  'Logout' : 'Login' }</button>
+      <section>
+
+
+        {/* <h2 onClick={()=>setFirstName(firstName + '*')}>Hello {firstName}</h2> */}
+      </section>
       </header>
+     
     </div>
   );
 }
